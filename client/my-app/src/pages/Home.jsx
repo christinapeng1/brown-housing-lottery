1f68fdb1ca3a6c811afc542465b992b1185c7404
kdolgopo <144499797+kdolgopo@users.noreply.github.com>
2024-05-11 14:44:30 -0400

    last commit

--- client/my-app/src/pages/Home.jsx
+++ client/my-app/src/pages/Home.jsx
@@ -167,7 +167,8 @@ export default function Home() {
           <option value="single">Single</option>
           <option value="double">Double</option>
           <option value="triple">Triple</option>
-          <option value="suite">Suite</option>
+          <option value="quad">Quad</option>
+          <option value="suite">Suite/Apartment</option>
         </select>
         <h3> Building:</h3>
         <input
